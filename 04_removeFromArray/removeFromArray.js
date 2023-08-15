89bc985c6df3c2e66b116abda523670c215cc764
BurkeAkcakil <98296072+BurkeAkcakil@users.noreply.github.com>
2023-08-15 11:46:50 -0400

    Completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,17 @@
-let myArray = [1, 2, 3, 4];
 
-const removeFromArray = function() {
 
+
+const removeFromArray = function(myArray, ...args) {
+    const newArray = [];
+    myArray.forEach(items => {
+        if (!args.includes(items)) {
+            newArray.push(items);
+        }
+        
+    });
+    return newArray;
 };
 
+console.log(removeFromArray([1,2,3,4],3));
 // Do not edit below this line
 module.exports = removeFromArray;
