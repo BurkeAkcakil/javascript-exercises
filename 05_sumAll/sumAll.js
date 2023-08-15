598b46859cb80803b49949fb8dcabf889209d22d
BurkeAkcakil <98296072+BurkeAkcakil@users.noreply.github.com>
2023-08-15 13:15:04 -0400

    Working through 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(no1, no2) {
+    let sum = 0; 
+    for (let i = no1; i <= no2; i++) {
+        sum += i;
+    }
+    return sum;  
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
